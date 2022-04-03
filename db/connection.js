8fcabdd8d084ac749a39b5083e14ad2734a4240c
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2022-04-03 17:38:22 +0530

    DB CONFIG SETUP

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,12 @@
 const mysql = require('mysql');
 const pg = require('pg')
 const Pool = require('pg').Pool;
+const dotenv = require('dotenv')
 
-const CON_STRING = 'postgres://zbifugiuxoyjwl:520ec0140798a4e4757f45fb9a92b4dcfe6290116bfe4b2407a0d8f2ae60fe74@ec2-34-207-12-160.compute-1.amazonaws.com:5432/d1dihgggmvp970';
+dotenv.config({path : '../config.env'})
+const CON_STRING = process.env.DB_URL || 'postgres://root:1234@localhost:5432/dictionary';
+console.log(CON_STRING)
+
 const pool = new Pool({
     connectionString: CON_STRING,
     // dialect: 'postgresql',
@@ -12,37 +16,10 @@ const pool = new Pool({
     }
 });
 
-pool.connect();
-
-
-// const pool = new Pool({
-//     user: 'zbifugiuxoyjwl',
-//     host: 'ec2-34-207-12-160.compute-1.amazonaws.com',
-//     database: 'd1dihgggmvp970',
-//     password: '520ec0140798a4e4757f45fb9a92b4dcfe6290116bfe4b2407a0d8f2ae60fe74',
-//     port: 5432, ssl: true
-// });
-
-// pool.connect();
-
-console.log('Database Connected.');
-
-
-
-// const db = mysql.createConnection({
-//     host: 'sql.freedb.tech',
-//     user : 'freedb_yuvraj',
-//     password: 'Nxk!JNMbkzBR#2?',
-//     database: 'freedb_dictionary'
-// })
-
-// db.connect((err) => {
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-
-//     console.log('DB Connected.')
-// })
+pool.connect().then((result)=> {
+    console.log('Database Connected.');
+}).catch((error) => {
+    console.log(error);
+})
 
 module.exports = pool;
