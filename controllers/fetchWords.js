5f287a30879a9b80396cd354217cd6019bc2881d
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2022-04-04 18:08:20 +0530

    schema changed

--- controllers/fetchWords.js
+++ controllers/fetchWords.js
@@ -55,36 +55,63 @@ exports.fetchDefinitionByWord = (req, res) => {
 
     const {languageCode, word} = req.body;
 
-    let sql = 'SELECT * FROM WORDS WHERE WORD = $1 AND LANGUAGE_CODE = $2';
+    if(languageCode == 'en'){
+        let sql = 'SELECT * FROM WORDS WHERE ENGLISH = $1';
 
-    let query = db.query(sql, [word, languageCode], (err, results) => {
-        if(err){
-            console.log(err);
-            return res.status(404).json({
-                'statusCode' : 404, 
-                'developerMessage' : 'Some Error Occurred.',
+        let query = db.query(sql, [word], (err, results) => {
+            if(err){
+                console.log(err);
+                return res.status(404).json({
+                    'statusCode' : 404, 
+                    'developerMessage' : 'Some Error Occurred.',
+                    'result' : results.rows
+                });
+            }
+
+            return res.status(200).json({
+                'statusCode' : 200,
+                'developerMessage' : 'Words fetched successfully.',
                 'result' : results.rows
-            });
-        }
-
-        // console.log(query['_results'][0]);
-        // var data = [];
-        // for(var x in query['_results'][0]){
-        //     // console.log(query['_results'][0][x]);
-        //     data.push({
-        //         'word' : query['_results'][0][x]['word'],
-        //         'languageCode' : query['_results'][0][x]['language_code'],
-        //         'definition': query['_results'][0][x]['definition']
-        //     });
-        // }
-
-        // console.log(data);
-
-        return res.status(200).json({
-            'statusCode' : 200,
-            'developerMessage' : 'Words fetched successfully.',
-            'result' : results.rows
+            })
         })
-    })
+    }else if(languageCode == 'hi'){
+        let sql = 'SELECT * FROM WORDS WHERE HINDI = $1';
+
+        let query = db.query(sql, [word], (err, results) => {
+            if(err){
+                console.log(err);
+                return res.status(404).json({
+                    'statusCode' : 404, 
+                    'developerMessage' : 'Some Error Occurred.',
+                    'result' : results.rows
+                });
+            }
+
+            return res.status(200).json({
+                'statusCode' : 200,
+                'developerMessage' : 'Words fetched successfully.',
+                'result' : results.rows
+            })
+        })
+    }else{
+        let sql = 'SELECT * FROM WORDS WHERE CHHATTISGARHI = $1';
+
+        let query = db.query(sql, [word], (err, results) => {
+            if(err){
+                console.log(err);
+                return res.status(404).json({
+                    'statusCode' : 404, 
+                    'developerMessage' : 'Some Error Occurred.',
+                    'result' : results.rows
+                });
+            }
+
+            return res.status(200).json({
+                'statusCode' : 200,
+                'developerMessage' : 'Words fetched successfully.',
+                'result' : results.rows
+            })
+        })
+    }
 
 }
